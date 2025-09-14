b73359dc82c9c26a9fe4fc01b710b12d069e0984
WanjakeKibe <wanjakejohn63@gmail.com>
2025-09-15 00:18:17 +0300

    Update basic_operators.py

--- basic_operators.py
+++ basic_operators.py
@@ -1,15 +1,14 @@
 number1 = 10
-number = 5
+number2 = 5
 
-//Calculating the sum
-sum = number1 + number2
-ptint('The sum of the two is',sum);
+# Calculating the sum
+result_sum = number1 + number2
+print(f'The sum of {number1} and {number2} is {result_sum}')
 
-//Calculatting the difference
-Def = number1 - number2
-print('The differnce of the two is',Def);
+# Calculating the difference
+difference = number1 - number2
+print(f'The difference between {number1} and {number2} is {difference}')
 
-//Calculating the product
-prod = number1 * number2
-print('The product of the two is',prod);
-
+# Calculating the product
+product = number1 * number2
+print(f'The product of {number1} and {number2} is {product}')
