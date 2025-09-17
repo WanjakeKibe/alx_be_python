a59d68822f73055b15e8cdf8210101ae8baf209a
WanjakeKibe <wanjakejohn63@gmail.com>
2025-09-17 13:48:02 +0300

    Delete alx_be_python directory

--- alx_be_python/future_age_calculator.py
+++
@@ -1,4 +0,0 @@
-age = int(input("How old are you? "))
-age_in_2050 = age + 27
-
-print(f"In 2050, you will be {age_in_2050} years old.")
--- alx_be_python/hours_to_seconds.py
+++
@@ -1,5 +0,0 @@
-hours = 2
-seconds = hours * 3600
-
-# Print the result
-print(f"{hours} hour(s) is {seconds} seconds")
