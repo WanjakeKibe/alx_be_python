2549f235d08dc95a08f4c4071767be247b3efeb5
WanjakeKibe <wanjakejohn63@gmail.com>
2025-09-17 12:06:26 +0300

    Add files via upload

---
+++ alx_be_python/basic_operations.py
@@ -0,0 +1,14 @@
+number1 = 10
+number2 = 5
+
+# Calculating the sum
+result_sum = number1 + number2
+print(f'The sum of {number1} and {number2} is {result_sum}')
+
+# Calculating the difference
+difference = number1 - number2
+print(f'The difference between {number1} and {number2} is {difference}')
+
+# Calculating the product
+product = number1 * number2
+print(f'The product of {number1} and {number2} is {product}')
---
+++ alx_be_python/finance_calculator.py
@@ -0,0 +1,10 @@
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
+
+monthly_savings = monthly_income - monthly_expenses
+
+annual_savings_without_interest = monthly_savings * 12
+projected_annual_savings = annual_savings_without_interest + (annual_savings_without_interest * 0.05)
+
+print(f"\nYour monthly savings: ${monthly_savings:,.2f}")
+print(f"Projected annual savings with 5% interest: ${projected_annual_savings:,.2f}")
