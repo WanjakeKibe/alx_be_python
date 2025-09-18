12104cecc15918339232a139b0bd31d4b71cbcf5
WanjakeKibe <wanjakejohn63@gmail.com>
2025-09-18 12:03:24 +0300

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,6 @@
- principal = 1000
- rate = 0.05
- time = 3
- i = principle * time * rate
+principal = 1000;
+rate = 0.05;
+time = 3;
+i = principal * time * rate
 print('The simple interest is',i);
 
